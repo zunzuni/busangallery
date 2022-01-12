ebf5dacb8177a39a4b72485266b8a3f13a6f139f
zunzuni <lovemehui12312@gmail.com>
2022-01-12 22:02:54 +0900

    kakao api

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ hamBtn.addEventListener('click',() => {
 
 var container = document.getElementById('map');
 var options = {
-  center: new kakao.maps.LatLng(33.450701, 126.570667),
+  center: new kakao.maps.LatLng(35.12762087089539, 128.97563060299737),
   level: 3
 };
 
